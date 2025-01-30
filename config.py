d47b4b8c9e484289263912c5ca329da91970fb1f
inksne <debanyj23@gmail.com>
2025-01-30 19:52:23 +0300

    получение переменных окружения и настройки для jwt и бд

---
+++ config.py
@@ -0,0 +1,32 @@
+from dotenv import load_dotenv, dotenv_values
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from pathlib import Path
+import os
+
+load_dotenv()
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_NAME = os.environ.get("DB_NAME")
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = Path("certs") / "jwt-private.pem"
+    public_key_path: Path = Path("certs") / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 5
+    refresh_token_expire_days: int = 30
+
+
+class Settings(BaseSettings):
+    auth_jwt: AuthJWT = AuthJWT()
+
+
+settings = Settings()
+
+class DBSettings(BaseSettings):
+    db_url: str = f'postgresql+asyncpg://{DB_USER}:{DB_PASS}@localhost:5432/{DB_NAME}'
+    db_echo: bool = False
+
+db_settings = DBSettings()
